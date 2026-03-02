6951b9dda7faedf3ba4c30afd1b05470e3234621
ericmpapa <me@ericmpapa.com>
2026-03-02 18:03:20 +0200

    update

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:test_flutter_2/pages/citation_page.dart';
+
+void main() {
+  runApp(MaterialApp(title: "citation", home: CitationPage()));
+}
---
+++ lib/pages/citation_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class CitationPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("citation"), centerTitle: true),
+      body: Container(color: Colors.blue),
+    );
+  }
+}
